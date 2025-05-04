990d88487319652b7d08ff313b904add9a28dc35
ReemNeama <neama.reem@gmail.com>
2025-05-05 01:52:26 +0300

    Remove milestone_provider.dart from root directory and update imports to use providers.dart barrel file

--- lib/screens/event_planning/milestones/milestone_screen.dart
+++ lib/screens/event_planning/milestones/milestone_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:eventati_book/models/models.dart';
-import 'package:eventati_book/providers/milestone_provider.dart';
+import 'package:eventati_book/providers/providers.dart';
 import 'package:eventati_book/styles/app_colors.dart';
 import 'package:eventati_book/styles/app_colors_dark.dart';
 import 'package:eventati_book/utils/utils.dart';
